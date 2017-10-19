c9010a1231a54e47ccc09f117aaa492966df13f7
xiong Mingcai <xmc000@icloud.com>
2017-10-19 14:31:34 +0800

    修正部署

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -35,7 +35,6 @@ module.exports = {
       // 因为pm2要登录到服务器（Server）中执行命令，所以要提供 name 和 host
       // 这里没有提供密码，也就是为什么要配置ssh免密码登录
       user : 'root',    // 服务器用户名
-      ssh_options: ["StrictHostKeyChecking=no"],
       host : '60.205.219.20',    // 服务器地址
       // 服务器（Server）需要获取GitHub上的仓库
       // 所以要配置Deploy Keys
@@ -43,8 +42,13 @@ module.exports = {
       // Github上的仓库地址
       repo : 'git@github.com:XiomgMingCai/pm2-zidong-bushu.git',
       path : '/root/www/pm2-zidong-bushu', // 应用部署到服务器的路径
+      "post-setup": "ls -la",
+      "ssh_options": "StrictHostKeyChecking=no",
       // 在服务器上执行的脚本命令，会在从Github上获取到最新的版本后执行
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      env  : {
+            "NODE_ENV": "production"
+      }
     },
     dev : {
       user : 'node',
--- server.js
+++ server.js
@@ -12,3 +12,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+//ssh-copy-id -i ~/.ssh/id_rsa.pub root@60.205.219.20
